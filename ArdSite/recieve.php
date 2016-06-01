cfad93eaa6cbfe21c6eb4a9bb472a63110224081
eli4 <eli4@users.noreply.github.com>
2016-06-01 15:14:28 -0700

    slightly updated webpage, added php

---
+++ ArdSite/recieve.php
@@ -0,0 +1,10 @@
+<?php
+    //$endTime = file_get_contents("put a path to the log file here");
+	$currentTime = time();
+	if($currentTime>EndTime){
+		//turn arduino off
+	}
+	else{
+		//leave arduino on
+	}
+?>
---
+++ ArdSite/server.php
@@ -0,0 +1,13 @@
+﻿<?php
+    $onOff = $_POST["onOff"];
+    $minutes = intval($_POST["time"]);
+    $endTime = time();
+    if($onOff == 'on'){
+      $endTime = time() + $minutes*60;
+      echo 'solenoid valve has been turned on for: ' . $minutes . ' minutes';
+    }
+    if($onOff == 'off'){
+      echo 'solenoid valve is turned off';
+    }
+    //file_put_contents("put a path for the log file here", $endTime);
+?>
