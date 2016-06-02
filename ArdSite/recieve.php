525913cfe33e012dd0fb290f77573ebb8d165c11
eli4 <eli4@users.noreply.github.com>
2016-06-02 14:22:45 -0700

    Update recieve.php

--- ArdSite/recieve.php
+++ ArdSite/recieve.php
@@ -2,9 +2,9 @@
     //$endTime = file_get_contents("put a path to the log file here");
 	$currentTime = time();
 	if($currentTime>$endTime){
-		//turn arduino off
+		echo ThisCodeTurnsTheUnoOff;
 	}
 	else{
-		//leave arduino on
+		echo ThisCodeTurnsTheUnoOnn;
 	}
 ?>
