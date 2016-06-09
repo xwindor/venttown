b627ca54774d5ba36c658adb56519a23a6d81ce0
eli4 <eli4@users.noreply.github.com>
2016-06-09 13:24:30 -0700

    Update recieve.php

--- ArdSite/recieve.php
+++ ArdSite/recieve.php
@@ -2,9 +2,9 @@
     //$endTime = file_get_contents("put a path to the log file here");
 	$currentTime = time();
 	if($currentTime>$endTime){
-		echo ThisCodeTurnsTheUnoOff;
+		echo "ThisCodeTurnsTheUnoOff";
 	}
 	else{
-		echo ThisCodeTurnsTheUnoOnn;
+		echo "ThisCodeTurnsTheUnoOnn";
 	}
 ?>
