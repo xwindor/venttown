54f5fdeba08b79989247460f62c9b6361a5304bb
Tyler Babin <t.babin9@gmail.com>
2016-06-13 13:34:41 -0400

    updated Rx and Tx code
    @xwindor I need you to check if the correct messages are being sent from
    the transmitter. I'll help you set it up.
    @eli4 let me know if you think of a good way to tell the transmitter
    which message it needs to send (valve on/valve off)

---
+++ Radio Code/receiver/nano_receiver/nano_receiver.ino
@@ -0,0 +1,51 @@
+/*
+ * Receive 4.1
+ * receives signal from Transmitter 4.2 to either turn on the valve or turn off the valve.
+ */
+
+#include <VirtualWire.h>
+
+char message[30];
+const int receivePin = 10;
+const int solenoidPin = 13;
+const int waterSensorPin = 4;
+
+void setup() {
+  pinMode(solenoidPin, OUTPUT);
+  
+  vw_set_tx_pin(receivePin);
+
+  vw_setup(2000);
+  //turns on the device for receiving messages.
+  vw_rx_start();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  uint8_t buf[VW_MAX_MESSAGE_LEN];
+  uint8_t bufflen = VW_MAX_MESSAGE_LEN;
+
+  if (vw_get_message(buf, &bufflen)) {
+    int i;
+
+    Serial.print("Got: ");
+
+    //TODO:
+    //make a way to check the string to see if it equals 'ValveOpening' or 'ValveClosing', and then perform the respective function
+    //XAVIER: I NEED YOU TO CHECK IF THIS WORKS, MSG ME NEXT TIME YOU'RE ON YOUR COMPUTER AND I'LL HELP YOU SET IT UP.
+    for (i = 0; i < bufflen; i++) {
+      Serial.print(buf[i]);
+    }
+    Serial.println();
+  }
+}
+
+void openValve() {
+  digitalWrite(solenoidPin, HIGH);
+}
+
+void closeValve() {
+  digitalWrite(solenoidPin, LOW);
+}
+
+
---
+++ Radio Code/transmitter/uno_transmitter/uno_transmitter.ino
@@ -0,0 +1,44 @@
+/*
+ * Transmitter 4.2
+ * sends signal from UNO to NANO to activate the solenoid valve
+ * 
+ * 
+ * for this I think we need some way of sending a flag to this transmitter in order to determine if we are sending the signal to
+ * open the valve or close the valve. Eli if you have any ideas on how to do this let me know.
+ */
+
+#include <VirtualWire.h>
+
+const int transmitPin = 11;
+char message[30];
+
+void setup() {
+  vw_setup(2000);
+  
+  //Not sure which pin you are going to use for the transmitter Xavier, the sample code says 12 is the default
+  vw_set_tx_pin(transmitPin);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  //activate();
+  //deactivate();
+}
+
+//sends the message to the receiver to activate the valve
+void activate() {
+  message = "OpeningValve";
+  vw_send((uint8_t*) message, strlen(message));
+  vw_wait_tx();
+  delay(1000);
+}
+
+//sends the message to the receiver to activate the valve
+void deactivate() {
+  message = "ClosingValve";
+  vw_send((uint8_t*) message, strlen(message));
+  vw_wait_tx();
+  delay(1000);
+}
+
