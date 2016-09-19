330c56c27d56df5d168b632ddddcbd28b28fc9ea
eli4 <eli4@users.noreply.github.com>
2016-09-19 15:39:24 -0700

    added css link

--- ArdSite/ArdOnOff.php
+++ ArdSite/ArdOnOff.php
@@ -1,3 +1,11 @@
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta charset="utf-8" />
+    	<link rel="stylesheet" type="text/css" href="./main.css" />
+        <title></title>
+    </head>
+</html>
+	
 <?php
     $endTime = file_get_contents("C:\\YOURPATH\\log.txt");
     $currentTime = time();
