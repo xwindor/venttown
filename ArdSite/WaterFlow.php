e58b4e3f3fc59f727ef1254ad6e5d76648df7131
eli4 <eli4@users.noreply.github.com>
2016-09-19 15:42:17 -0700

    added css link

--- ArdSite/WaterFlow.php
+++ ArdSite/WaterFlow.php
@@ -1,6 +1,15 @@
-<a href="Home.html">Home</a>
-<a href="WaterFlow.php">Water Flow</a>
-<br>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta charset="utf-8" />
+    	<link rel="stylesheet" type="text/css" href="./main.css" />
+        <title></title>
+    </head>
+    <body>
+        <a href="Home.html">Home</a>
+        <a href="WaterFlow.php">Water Flow</a>
+        <br>
+    </body>
+</html>
 <?php
 	$flow = file_get_contents("C:\\YOURPATH\\flow.txt");
 	echo $flow;
