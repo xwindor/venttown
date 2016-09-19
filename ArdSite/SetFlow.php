3073d9f91cfaa3dae033dda52f84f6453c9b9680
eli4 <eli4@users.noreply.github.com>
2016-09-19 15:41:12 -0700

    added css link

--- ArdSite/SetFlow.php
+++ ArdSite/SetFlow.php
@@ -1,3 +1,10 @@
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta charset="utf-8" />
+    	<link rel="stylesheet" type="text/css" href="./main.css" />
+        <title></title>
+    </head>
+</html>
 <?php
     $flow = $_GET["flow"];
     file_put_contents("C:\\YOURPATH\\flow.txt", $flow);
