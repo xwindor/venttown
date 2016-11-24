18e8fdbd3f1e125e13fec97379d8a2e8641686f3
xwindor <xavierpestana@outlook.com>
2016-11-23 22:37:26 -0500

    removed unnescary things

--- ArdSite/wokring_valve_sketch/wokring_valve_sketch.ino
+++ ArdSite/wokring_valve_sketch/wokring_valve_sketch.ino
@@ -32,8 +32,8 @@ Distributed as-is; no warranty is given.
 const char mySSID[] = "li";
 const char myPSK[] = "6689581li";
 const int solenoidValve = 13;
-const int blinker = 4; //pin for testing this pin will blink
-const int wifiStatus = 7; // if on LED is lit
+const int serverConnectStatus = 4; // on if arduino is connected to server GREEN
+const int wifiStatus = 7; // if on LED is lit ORANGE
 char onCode[23] = "ThisCodeTurnsTheUnoOnn";
 char offCode[23] = "ThisCodeTurnsTheUnoOff";
 
@@ -44,8 +44,6 @@ unsigned char flowsensor = 2; // Sensor Input
 unsigned long currentTime;
 unsigned long cloopTime;
 
-//test vars
-int t=0;
 
 //////////////////
 // HTTP Strings //
@@ -61,38 +59,25 @@ const String httpRequest = "GET /ArdOnOff.php HTTP/1.1\n"
 // copy/paste into sketches of your own.
 void setup() 
 {
-   pinMode(flowsensor, INPUT);
-   pinMode(blinker, OUTPUT);
-   pinMode(wifiStatus, OUTPUT);
-   pinMode(solenoidValve, OUTPUT);
-   digitalWrite(flowsensor, HIGH); // Optional Internal Pull-Up
-   Serial.begin(9600);
-   attachInterrupt(0, flow, RISING); // Setup Interrupt
-   sei(); // Enable interrupts
-   currentTime = millis();
-   cloopTime = currentTime;
-   
+  pinMode(flowsensor, INPUT);
+  pinMode(serverConnectStatus, OUTPUT);
+  pinMode(wifiStatus, OUTPUT);
+  pinMode(solenoidValve, OUTPUT);
+  digitalWrite(flowsensor, HIGH); // Optional Internal Pull-Up
   Serial.begin(9600);
-
+  attachInterrupt(0,flow, RISING); //Setup Interupt
+  sei(); // Enable interrupts
+  currentTime = millis();
+  cloopTime = currentTime;
+  Serial.begin(9600);
   initializeESP8266();
   valveControl();
+  connectESP8266();
 }
 
 void loop() 
 {
-  if (t ==20){
-    esp8266.disconnect();
-    digitalWrite(wifiStatus, LOW);
-  }
-  if(t == 40){
-    connectESP8266();
-    t=0;
-  }
-  digitalWrite(blinker, HIGH);
-  delay(500);
-  digitalWrite(blinker, LOW);
-  delay(500);
-  t++;
+  
   setFlow();
   valveControl();
 }
@@ -104,92 +89,24 @@ void initializeESP8266()
   // It returns either true or false -- indicating whether
   // communication was successul or not.
   // true
-  int test = esp8266.begin();
-  if (test != true)
-  {
-    Serial.println(F("Error talking to ESP8266."));
-    errorLoop(test);
+  esp8266.begin();
   }
-  Serial.println(F("ESP8266 Shield Present"));
-}
 
 void connectESP8266()
 {
-  // The ESP8266 can be set to one of three modes:
-  //  1 - ESP8266_MODE_STA - Station only
-  //  2 - ESP8266_MODE_AP - Access point only
-  //  3 - ESP8266_MODE_STAAP - Station/AP combo
-  // Use esp8266.getMode() to check which mode it's in:
-  int retVal = esp8266.getMode();
-  if (retVal != ESP8266_MODE_STA)
-  { // If it's not in station mode.
-    // Use esp8266.setMode([mode]) to set it to a specified
-    // mode.
-    retVal = esp8266.setMode(ESP8266_MODE_STA);
-    if (retVal < 0)
-    {
-      Serial.println(F("Error setting mode."));
-      errorLoop(retVal);
-    }
-  }
-  Serial.println(F("Mode set to station"));
-
-  // esp8266.status() indicates the ESP8266's WiFi connect
-  // status.
-  // A return value of 1 indicates the device is already
-  // connected. 0 indicates disconnected. (Negative values
-  // equate to communication errors.)
-  retVal = esp8266.status();
-  if (retVal <= 0)
-  {
-    Serial.print(F("Connecting to "));
-    Serial.println(mySSID);
     // esp8266.connect([ssid], [psk]) connects the ESP8266
     // to a network.
     // On success the connect function returns a value >0
     // On fail, the function will either return:
     //  -1: TIMEOUT - The library has a set 30s timeout
     //  -3: FAIL - Couldn't connect to network.
-    retVal = esp8266.connect(mySSID, myPSK);
-    if (retVal < 0)
-    {
-      Serial.println(F("Error connecting"));
-      errorLoop(retVal);
-    }
+    int retVal = esp8266.connect(mySSID, myPSK);
     if (retVal > 0)
   {
     digitalWrite(wifiStatus, HIGH);
   }
-  }
-}
-
-void displayConnectInfo()
-{
-  char connectedSSID[24];
-  memset(connectedSSID, 0, 24);
-  // esp8266.getAP() can be used to check which AP the
-  // ESP8266 is connected to. It returns an error code.
-  // The connected AP is returned by reference as a parameter.
-  int retVal = esp8266.getAP(connectedSSID);
-  if (retVal > 0)
-  {
-    Serial.print(F("Connected to: "));
-    Serial.println(connectedSSID);
-  }
-
-  // esp8266.localIP returns an IPAddress variable with the
-  // ESP8266's current local IP address.
-  IPAddress myIP = esp8266.localIP();
-  Serial.print(F("My IP: ")); Serial.println(myIP);
-}
-
-void flow () // Interrupt function
-{
-   flow_frequency++;
-}
-
-void setFlow(){
-   // To use the ESP8266 as a TCP client, use the 
+  
+    // To use the ESP8266 as a TCP client, use the 
   // ESP8266Client class. First, create an object:
   ESP8266Client client;
 
@@ -198,14 +115,23 @@ void setFlow(){
   // a specified port.
   // Returns: 1 on success, 2 on already connected,
   // negative on fail (-1=TIMEOUT, -3=FAIL).
-  int retVal = client.connect(destServer, 8001);
-  if (retVal <= 0)
+  retVal = client.connect(destServer, 80);
+  if (retVal > 0)
   {
-    Serial.println(F("Failed to connect to server."));
+    digitalWrite(serverConnectStatus, HIGH);
     return;
   }
+}
 
-   currentTime = millis();
+void flow () // Interrupt function
+{
+   flow_frequency++;
+}
+
+void setFlow()
+{
+    ESP8266Client client;
+    currentTime = millis();
    // Every second, calculate and print litres/hour
    if(currentTime >= (cloopTime + 1000))
    {
@@ -221,28 +147,11 @@ void setFlow(){
                            "Host: http://xaviersgarden.com/Home.php\n" //private ip
                            "Connection: close\n\n";
   client.print(httpPostRequest);
-  if (client.connected())
-    client.stop(); // stop() closes a TCP connection.  
 }
 
 void valveControl()
 {
-  // To use the ESP8266 as a TCP client, use the 
-  // ESP8266Client class. First, create an object:
   ESP8266Client client;
-
-  // ESP8266Client connect([server], [port]) is used to 
-  // connect to a server (const char * or IPAddress) on
-  // a specified port.
-  // Returns: 1 on success, 2 on already connected,
-  // negative on fail (-1=TIMEOUT, -3=FAIL).
-  int retVal = client.connect(destServer, 8001);
-  if (retVal <= 0)
-  {
-    Serial.println(F("Failed to connect to server."));
-    return;
-  }
- 
 
   // print and write can be used to send data to a connected
   // client connection.
@@ -275,19 +184,6 @@ void valveControl()
     Serial.write(c);
   }
   
-  // connected() is a boolean return value - 1 if the 
-  // connection is active, 0 if it's closed.
-  if (client.connected())
-    client.stop(); // stop() closes a TCP connection.
-}
-  
-// errorLoop prints an error code, then loops forever.
-void errorLoop(int error)
-{
-  Serial.print(F("Error: ")); Serial.println(error);
-  Serial.println(F("Looping forever."));
-  for (;;)
-    ;
 }
 
 
